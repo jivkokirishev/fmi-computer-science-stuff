59f2009f9fb73fd4d4d6d5c65ec2b86f5bc911e5
jivkokirishev <jivkokirishev@gmail.com>
2019-12-03 12:10:47 +0200

    some unit tests added

--- functional-programming/homeworks/big-homework-1/03-dates-and-events.rkt
+++ functional-programming/homeworks/big-homework-1/03-dates-and-events.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(require rackunit)
+(require rackunit/text-ui)
 
 (define (make-date day month year)
   (list day month year))
@@ -14,9 +16,9 @@
 
 (define (leapYear? date)
   (cond
-    ((= (remainder (year date) 400)) #t)
-    ((= (remainder (year date) 100)) #f)
-    ((= (remainder (year date) 4)) #t)
+    ((= (remainder (year date) 400) 0) #t)
+    ((= (remainder (year date) 100) 0) #f)
+    ((= (remainder (year date) 4) 0) #t)
     (else #f)))
 
 (define (partOutside? date part low up)
@@ -119,3 +121,111 @@
     (date< (car events1) (car events2)))
   
   (sort (calendarFiller '() events) eventsSortPred?))
+
+
+(define make-date-tests
+  (test-suite "make-date tests"
+    (check-equal? (make-date 1 12 2019) '(1 12 2019))
+    (check-equal? (make-date 11 6 -230) '(11 6 -230))
+    (check-equal? (make-date 25 3 2158) '(25 3 2158))
+  )
+)
+
+(define day-month-year-tests
+  (test-suite "day month year tests"
+    (check-equal? (day (make-date 21 11 2019)) 21)
+    (check-equal? (month (make-date 21 11 2019)) 11)
+    (check-equal? (year (make-date 21 11 2019)) 2019)
+  )
+)
+
+(define date?-tests
+  (test-suite "date? tests"
+    (check-true (date? (make-date 21 11 2019)))
+    (check-true (date? (make-date 21 11 -2019)))
+    (check-false (date? (make-date 51 11 2019)))
+    (check-false (date? (make-date 21 13 2019)))
+    (check-false (date? '(21 13 2019 424)))
+    (check-false (date? '(21 'Jan 2019)))
+    (check-false (date? '(21 13)))
+    (check-true (date? '(10 9 2009)))
+    (check-false (date? (make-date 29 2 2017)))
+    (check-true (date? (make-date 29 2 2016)))
+    (check-true (date? (make-date 31 12 2019)))
+    (check-true (date? (make-date 30 11 2019)))
+    (check-false (date? (make-date 31 11 2019)))
+    (check-false (date? (make-date -2 11 2019)))
+    (check-false (date? (make-date 2 -11 2019)))
+    
+  )
+)
+
+(define date->string-tests
+  (test-suite "date->string tests"
+    (check-equal? (date->string (make-date 21 11 2019)) "21.11.2019")
+    (check-equal? (date->string (make-date 31 12 -2019)) "31.12.-2019")
+    (check-equal? (date->string (make-date 1 1 1)) "1.1.1")
+  )
+)
+                  
+(define next-day-tests
+  (test-suite "next-day tests"
+    (check-equal?  (next-day (make-date 21 11 2019)) '(22 11 2019))
+    (check-equal? (next-day (make-date 30 11 2019)) '(1 12 2019))
+    (check-equal? (next-day (make-date 31 12 2019)) '(1 1 2020))
+    (check-equal? (next-day (make-date 28 2 2016)) '(29 2 2016))
+    (check-equal? (next-day (make-date 29 2 2016)) '(1 3 2016))
+    (check-equal? (next-day (make-date 28 2 2015)) '(1 3 2015))
+  )
+)
+
+(define date<-tests
+  (test-suite "date< tests"
+    (check-true (date< (make-date 21 11 2019) (make-date 1 1 2020)))
+    (check-false (date< (make-date 21 11 2019) (make-date 1 1 2019)))
+    (check-true (date< (make-date 21 11 2019) (make-date 22 11 2019)))
+    (check-true (date< (make-date 21 11 2019) (make-date 21 12 2019)))
+    (check-false (date< (make-date 29 2 2016) (make-date 29 2 2016)))
+    (check-false (date< (make-date 29 2 2016) (make-date 28 2 2016)))
+  )
+)
+
+(define weekday-tests
+  (test-suite "weekday tests"
+    (check-equal? (weekday (make-date 21 11 2019)) 'Thursday)
+    (check-equal? (weekday (make-date 22 11 2019)) 'Friday)
+;    (check-equal? (weekday (make-date 15 3 2018)) 'Wednesday)  ???
+    (check-equal? (weekday (make-date 13 4 2020)) 'Monday)
+  )
+)
+
+(define next-weekday-tests
+  (test-suite "next-weekday tests"
+    (check-equal? (next-weekday 'Thursday (make-date 21 11 2019)) '(21 11 2019))
+    (check-equal? (next-weekday 'Tuesday (make-date 21 11 2019)) '(26 11 2019))
+    (check-equal? (next-weekday 'Monday (make-date 10 4 2020)) '(13 4 2020))
+;    (check-equal? (next-weekday 'Saturday (make-date 24 5 2020)) '(24 5 2020))  ???
+  )
+)
+
+(define any?-tests
+  (test-suite "any? tests"
+    (check-true (any? odd? '(2 4 4 2 8 9 2 0)))
+    (check-true (any? (lambda (x) (> (length x) 3)) '((1 2 3) (3 4 4 2) (2 1))))
+    (check-false (any? (lambda (x) (> x 2)) (map (lambda (x) (remainder x 3)) (range 1 100))))
+  )
+)
+
+
+
+
+(run-tests make-date-tests 'verbose)
+(run-tests day-month-year-tests 'verbose)
+(run-tests date?-tests 'verbose)
+(run-tests date->string-tests 'verbose)
+(run-tests next-day-tests 'verbose)
+(run-tests date<-tests 'verbose)
+(run-tests weekday-tests 'verbose)
+(run-tests next-weekday-tests 'verbose)
+
+(run-tests any?-tests 'verbose)
